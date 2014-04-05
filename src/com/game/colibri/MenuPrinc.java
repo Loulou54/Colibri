c28a93785c22cf344445cf0f4bf7e04c5ff162f4
Loulou54 <louis.durand@telecom-bretagne.eu>
2014-04-05 15:41:50 +0200

    Colibri - Développement
    Projet Développement Telecom Bretagne

---
+++ src/com/game/colibri/MenuPrinc.java
@@ -0,0 +1,28 @@
+package com.game.colibri;
+
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.app.Activity;
+
+public class MenuPrinc extends Activity {
+	
+	public int ww,wh;
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu_princ);
+		Display display = getWindowManager().getDefaultDisplay();
+		ww = display.getWidth();
+		wh = display.getHeight();
+		Button btn_lay = (Button)findViewById(R.id.bout1);
+		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) btn_lay.getLayoutParams();
+		layoutParams.leftMargin = ww*4/9;
+	    layoutParams.topMargin = wh/3;
+	    btn_lay.setLayoutParams(layoutParams);
+	}
+
+}
