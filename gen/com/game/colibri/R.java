d3366e50871a69e1f16317c8b2c0ac1093e31db7
Loulou54 <louis.durand@telecom-bretagne.eu>
2014-04-07 02:00:02 +0200

    test

--- gen/com/game/colibri/R.java
+++ gen/com/game/colibri/R.java
@@ -27,8 +27,11 @@ public final class R {
         public static final int bouton_normal=0x7f020001;
         public static final int bouton_pressed=0x7f020002;
         public static final int boutons=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int menu=0x7f020005;
+        public static final int fleur=0x7f020004;
+        public static final int fleurm=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int menhir=0x7f020007;
+        public static final int menu=0x7f020008;
     }
     public static final class id {
         public static final int bout1=0x7f070001;
