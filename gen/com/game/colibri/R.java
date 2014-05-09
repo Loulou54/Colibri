65278f69b35f1beabcaa7de525105b40cd83fe9e
marthiam <Mariam@macthiam.maisel.enst-bretagne.fr>
2014-05-09 14:28:22 +0200

    conflit

--- gen/com/game/colibri/R.java
+++ gen/com/game/colibri/R.java
@@ -29,81 +29,48 @@ public final class R {
         public static final int btns_width=0x7f060002;
     }
     public static final class drawable {
-<<<<<<< HEAD
         public static final int arrier=0x7f020000;
-        public static final int bouton_enabled=0x7f020001;
-        public static final int bouton_normal=0x7f020002;
-        public static final int bouton_pressed=0x7f020003;
-        public static final int boutons=0x7f020004;
-        public static final int button=0x7f020005;
-        public static final int button_enable=0x7f020006;
-        public static final int button_normal=0x7f020007;
-        public static final int button_pressed=0x7f020008;
-        public static final int colibri_d=0x7f020009;
-        public static final int colibri_d1=0x7f02000a;
-        public static final int colibri_d2=0x7f02000b;
-        public static final int fleur=0x7f02000c;
-        public static final int fleurm=0x7f02000d;
-        public static final int herbe=0x7f02000e;
-        public static final int ic_launcher=0x7f02000f;
-        public static final int menhir=0x7f020010;
-        public static final int menu=0x7f020011;
-        public static final int vache=0x7f020012;
-        public static final int vache_0=0x7f020013;
-        public static final int vache_rd=0x7f020014;
-        public static final int vache_rg=0x7f020015;
-    }
-    public static final class id {
-        public static final int bout1=0x7f080003;
-        public static final int bout2=0x7f080004;
-        public static final int bout3=0x7f080005;
-        public static final int bout4=0x7f080006;
-        public static final int bout5=0x7f080007;
-        public static final int but1=0x7f080008;
-        public static final int but2=0x7f080009;
-        public static final int but3=0x7f08000a;
-        public static final int but4=0x7f08000b;
-        public static final int carte=0x7f080001;
-        public static final int lay=0x7f080000;
-        public static final int root=0x7f080002;
-=======
-        public static final int bouton_dyna=0x7f020000;
-        public static final int bouton_dynamite=0x7f020001;
-        public static final int bouton_dynamite_select=0x7f020002;
-        public static final int bouton_enabled=0x7f020003;
-        public static final int bouton_normal=0x7f020004;
-        public static final int bouton_pressed=0x7f020005;
-        public static final int boutons=0x7f020006;
-        public static final int chat=0x7f020007;
-        public static final int chat_0=0x7f020008;
-        public static final int chat_rd=0x7f020009;
-        public static final int chat_rg=0x7f02000a;
-        public static final int colibri_d=0x7f02000b;
-        public static final int colibri_d1=0x7f02000c;
-        public static final int colibri_d2=0x7f02000d;
-        public static final int dynamite=0x7f02000e;
-        public static final int dynamite_allumee=0x7f02000f;
-        public static final int explo0=0x7f020010;
-        public static final int explo1=0x7f020011;
-        public static final int explo2=0x7f020012;
-        public static final int explo3=0x7f020013;
-        public static final int explo4=0x7f020014;
-        public static final int explo5=0x7f020015;
-        public static final int explosion=0x7f020016;
-        public static final int fleur=0x7f020017;
-        public static final int fleurm=0x7f020018;
-        public static final int herbe=0x7f020019;
-        public static final int ic_launcher=0x7f02001a;
-        public static final int menhir=0x7f02001b;
-        public static final int menhir_rouge=0x7f02001c;
-        public static final int menu=0x7f02001d;
-        public static final int rainbow=0x7f02001e;
-        public static final int sang=0x7f02001f;
-        public static final int skull=0x7f020020;
-        public static final int vache=0x7f020021;
-        public static final int vache_0=0x7f020022;
-        public static final int vache_rd=0x7f020023;
-        public static final int vache_rg=0x7f020024;
+        public static final int bouton_dyna=0x7f020001;
+        public static final int bouton_dynamite=0x7f020002;
+        public static final int bouton_dynamite_select=0x7f020003;
+        public static final int bouton_enabled=0x7f020004;
+        public static final int bouton_normal=0x7f020005;
+        public static final int bouton_pressed=0x7f020006;
+        public static final int boutons=0x7f020007;
+        public static final int button=0x7f020008;
+        public static final int button_enable=0x7f020009;
+        public static final int button_normal=0x7f02000a;
+        public static final int button_pressed=0x7f02000b;
+        public static final int chat=0x7f02000c;
+        public static final int chat_0=0x7f02000d;
+        public static final int chat_rd=0x7f02000e;
+        public static final int chat_rg=0x7f02000f;
+        public static final int colibri_d=0x7f020010;
+        public static final int colibri_d1=0x7f020011;
+        public static final int colibri_d2=0x7f020012;
+        public static final int dynamite=0x7f020013;
+        public static final int dynamite_allumee=0x7f020014;
+        public static final int explo0=0x7f020015;
+        public static final int explo1=0x7f020016;
+        public static final int explo2=0x7f020017;
+        public static final int explo3=0x7f020018;
+        public static final int explo4=0x7f020019;
+        public static final int explo5=0x7f02001a;
+        public static final int explosion=0x7f02001b;
+        public static final int fleur=0x7f02001c;
+        public static final int fleurm=0x7f02001d;
+        public static final int herbe=0x7f02001e;
+        public static final int ic_launcher=0x7f02001f;
+        public static final int menhir=0x7f020020;
+        public static final int menhir_rouge=0x7f020021;
+        public static final int menu=0x7f020022;
+        public static final int rainbow=0x7f020023;
+        public static final int sang=0x7f020024;
+        public static final int skull=0x7f020025;
+        public static final int vache=0x7f020026;
+        public static final int vache_0=0x7f020027;
+        public static final int vache_rd=0x7f020028;
+        public static final int vache_rg=0x7f020029;
     }
     public static final class id {
         public static final int bout1=0x7f090004;
@@ -112,10 +79,13 @@ public final class R {
         public static final int bout4=0x7f090007;
         public static final int bout5=0x7f090008;
         public static final int bout_dyna=0x7f090002;
+        public static final int but1=0x7f090009;
+        public static final int but2=0x7f09000a;
+        public static final int but3=0x7f09000b;
+        public static final int but4=0x7f09000c;
         public static final int carte=0x7f090001;
         public static final int lay=0x7f090000;
         public static final int root=0x7f090003;
->>>>>>> FETCH_HEAD
     }
     public static final class layout {
         public static final int activity_jeu=0x7f030000;
@@ -126,18 +96,6 @@ public final class R {
         public static final int niveaux=0x7f050000;
     }
     public static final class string {
-<<<<<<< HEAD
-        public static final int aleat=0x7f060003;
-        public static final int app_name=0x7f060000;
-        public static final int campagne=0x7f060002;
-        public static final int content1=0x7f060006;
-        public static final int instructions=0x7f060005;
-        public static final int jouer=0x7f060001;
-        public static final int multi=0x7f060004;
-        public static final int recommencer=0x7f060008;
-        public static final int reprendre=0x7f060007;
-        public static final int sortir=0x7f060009;
-=======
         public static final int aleat=0x7f070003;
         public static final int app_name=0x7f070000;
         public static final int campagne=0x7f070002;
@@ -145,7 +103,9 @@ public final class R {
         public static final int instructions=0x7f070005;
         public static final int jouer=0x7f070001;
         public static final int multi=0x7f070004;
->>>>>>> FETCH_HEAD
+        public static final int recommencer=0x7f070008;
+        public static final int reprendre=0x7f070007;
+        public static final int sortir=0x7f070009;
     }
     public static final class style {
         /** 
